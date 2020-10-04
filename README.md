1c0410191eaad2bba26c4ab52593c8fbe7954dcc
Guilherme Silva <pereiraguilherme997@gmail.com>
2020-10-04 20:27:18 -0300

    Creating button component and update readme file

--- README.md
+++ README.md
@@ -12,15 +12,45 @@ npm install --save olimpo
 
 ## Usage
 
+### Button
+| Property | Description | Values |
+|----------|--------|--------|
+|text      | Text that will be displayed | **string**|
+|color     | Button color | Standart HTML color name: `'red', 'green', 'blue', 'yellow'...`<br> RGB code. Ex.: `rgb(255,0,0)` <br> Hexadecimal color code. Ex.: `#FF0000`|
+|textColor | Change the color of the text displayed | Standart HTML color name: `'red', 'green', 'blue', 'yellow'...`<br> RGB code. Ex.: `rgb(255,0,0)` <br> Hexadecimal color code. Ex.: `#ff0000`|
+|size      | Component size | Default sizes: `small, medium, large`<br> Number value for size in pixels. Ex.: `100px` |
+|shape     | Component and borders shape | `round, square, circle`|
+|disable   | Disable button flag | `'true' or 'false'`|
+|framework | Framework that component will be used when instanciated | `'antd' or 'materialui'` |
+|onClick   | Callback method for click event| **function**|
+|onMouseOver | Callback method for mouse over the component event | **function**|
+
+
 ```jsx
 import React, { Component } from 'react'
-
-import MyComponent from 'olimpo'
-import 'olimpo/dist/index.css'
+import { Button } from 'olimpo'
 
 class Example extends Component {
+
+  onClick = () => {
+    console.log("On click event");
+  }
+
+  onMouseOver = () => {
+    console.log("On mouse over event");
+  }
+
   render() {
-    return <MyComponent />
+    return <Button
+        text="My text" 
+        onClick={this.onClick()}
+        onMouseOver={this.onMouseOver()}
+        size={"large"}
+        disabled={false}
+        color={"red"}
+        textColor={"yellow"}
+        shape={"round"}
+     />
   }
 }
 ```
@@ -31,6 +61,7 @@ To import the module from local files, you need to import `olimpo` on your proje
 npm start ./olimpo
 npm start ./my-test-project
 ``` 
+
 ## License
 
 MIT © [pereiraguilherme](https://github.com/pereiraguilherme)
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,74 @@
 import React, { Component } from 'react'
+import './css/button.css'
 
 export default class Button extends Component {
-    render() {
-        return <button>Click me!</button>;
+    constructor(props) {
+        super(props);
+    }
+
+    onClick = () => {
+        if (this.props.onClick) {
+            return this.props.onClick
+        }
+    }
+
+    onMouseOver = () => {
+        if (this.props.onMouseOver) {
+            return this.props.onMouseOver
+        }
+    }
+
+    shouldReturnSize = (size) => {
+        if (isNaN(size)){
+            switch (size) {
+                case "small":
+                    return 150;
+                case "medium":
+                    return 200;
+                case "large":
+                    return 300
+                default:
+                    break;
+            }
+        } else {
+            return size;
+        }
+    }
+
+    shouldReturnShape = (shape) => {
+        switch (shape) {
+            case "round":
+                return {borderRadius: 12};
+            case "circle":
+                const radius = !this.props.circleRadius ? 75 : this.props.circleRadius;
+                return {
+                    borderRadius: (radius / 2),
+                    width: radius,
+                    height: radius,
+                }
+            default:
+                return {borderRadius: 3}
+        } 
+    }
+
+    render() {   
+        return (
+            <button 
+            className="button"
+            style={{
+                width: this.shouldReturnSize(this.props.size),
+                backgroundColor: this.props.color,
+                color: this.props.textColor,
+                ...this.shouldReturnShape(this.props.shape)
+            }}
+            disabled={this.props.disabled}
+            onClick={this.onClick()}
+            onMouseOver={this.onMouseOver()}
+            >
+                {!this.props.text ? 'Placeholder' : this.props.text}
+            
+            </button>
+            
+        );
     }
 }
