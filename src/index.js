529eaab070cc372b19b339c7640e9aa06127603c
Guilherme Silva <pereiraguilherme997@gmail.com>
2020-11-30 01:12:47 -0300

    Exporting component Card on index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Button from './components/Button'
 import Input from './components/Input'
 import Avatar from './components/Avatar'
 import Checkbox from './components/Checkbox'
+import Card from './components/Card'
 import './index.css'
 
-export { Button, Input, Avatar, Checkbox }
+export { Avatar, Button, Card, Checkbox, Input }
